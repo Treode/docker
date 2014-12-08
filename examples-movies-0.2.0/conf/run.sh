3157f766aabeb67921bf0061b66a8b536b5005b3
BrainFlame <BrainFlame@users.noreply.github.com>
2014-12-07 21:55:50 -0800

    Move the container to treode/exaxmples-movie:0.2.0
    Improve the run.sh and provide options to run in a cluster

--- examples-movies-0.2.0/conf/run.sh
+++ examples-movies-0.2.0/conf/run.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
 service nginx start
-H=`hostname -i | sed -e 's/\./ /g' |xargs printf '%02X'`
-echo "Initialize movies $H"
-java -jar /var/lib/treode/lib/movies-server-0.2.0.jar init -host=0x$H -cell=0x$H /var/lib/treode/db/store.3kv
-echo "starting movies."
+H=0x`hostname -i | sed -e 's/\./ /g' |xargs printf '%02X'`
+CELL=$H
+HAIL=""
+SOLO="-solo"
+IP=`hostname -i`
 
-java -jar /var/lib/treode/lib/movies-server-0.2.0.jar serve -solo /var/lib/treode/db/store.3kv  > /var/lib/treode/logs/server.log 2>&1 &
+while getopts "c:r:m" opt; do
+   case "$opt" in
+   c)   CELL=${OPTARG}
+        ;;
+   r)   HAIL=${OPTARG}
+        SOLO=""
+        ;;
+   m)   SOLO=""
+        ;;
+   esac
+done
+
+echo "Initializing movies - HOST ${H} (${IP}) - CELL ${CELL}"
+java -jar /var/lib/treode/lib/movies-server-0.2.0.jar init -host=${H} -cell=${CELL} /var/lib/treode/db/store.3kv
+
+echo "starting movies."
+if [ "X${HAIL}X" != "XX" ]; then
+   echo "HAILING -> ${HAIL}"
+fi
+
+java -jar /var/lib/treode/lib/movies-server-0.2.0.jar serve ${SOLO} ${HAIL} /var/lib/treode/db/store.3kv  > /var/lib/treode/logs/server.log 2>&1 &
 
 /bin/bash
 
